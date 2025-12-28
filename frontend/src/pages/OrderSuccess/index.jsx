c8f0604f8a0234ab3aca314a205a6296f17e7ed7
Nazarick0912 <gooyongshen12@gmail.com>
2025-12-28 18:29:33 +0800

    update: get the real order id passed from detailsform insted of fake mock id

--- frontend/src/pages/OrderSuccess/index.jsx
+++ frontend/src/pages/OrderSuccess/index.jsx
@@ -3,7 +3,8 @@ import SuccessContent from './components/SuccessContent';
 
 export default function OrderSuccess() {
     const location = useLocation();
-    const orderId = location.state?.orderId || "9fc96c9a-9c35-4cbd-a050-3480995766a2";
+    //get the real order id passed from detailsform insted of fake mock id
+    const orderId = location.state?.orderId;
 
     return <SuccessContent orderId={orderId} />;
 }
